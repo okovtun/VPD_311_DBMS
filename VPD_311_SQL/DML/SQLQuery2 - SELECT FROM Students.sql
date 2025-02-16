3ad92f5882fe14161be27d8bf8462aa22c18ef45
User <User@DESKTOP-0TCDSR6>
2025-02-16 16:48:37 +0300

    DML & Transact-SQL

---
+++ VPD_311_SQL/DML/SQLQuery2 - SELECT FROM Students.sql
@@ -0,0 +1,20 @@
+USE VPD_311_Import;
+GO
+
+SELECT 
+		last_name		AS	N'Фамилия',
+		first_name		AS	N'Имя',
+		middle_name		AS	N'Отество',
+		birth_date		AS	N'Дата рождения',
+		DATEDIFF(DAY, birth_date, GETDATE())/365 AS N'Возраст',
+		group_name		AS	N'Группа',
+		direction_name	AS	N'Направление обечения'
+FROM	Students, Groups, Directions
+WHERE	[group]			=	group_id
+AND		direction		=	direction_id
+--AND		direction_name	LIKE N'Разработка%'
+--ORDER BY last_name
+--ORDER BY birth_date		DESC --Descending (По убыванию)
+					--	ASC  --Ascending  (По возрастанию)
+ORDER BY	N'Возраст'	DESC
+;
